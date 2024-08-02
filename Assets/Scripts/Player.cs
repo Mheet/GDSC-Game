7ad079e7d426aa96b52d5a336130734351307206
Mheet Singh Bhaad <mheetsingh2005@gmail.com>
2024-08-02 16:47:31 +0530

    Project
    GDSC Project Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject loseScreen;
+    public Text healthDisplay;
+    public Text timeDisplay; 
+
+    public int health;
+    public float speed;
+    float input;
+    public float moveSmoothing = 0.1f;
+    public float stopDistance = 0.1f;
+
+    float timeSpent = 0f;
+
+    Animator anim;
+    Rigidbody2D rb;
+    AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        healthDisplay.text = health.ToString();
+    }
+
+    void Update()
+    {
+        timeSpent += Time.deltaTime;
+        timeDisplay.text = "Time: " + (int)timeSpent;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            float distanceToTouch = Mathf.Abs(touchPosition.x - transform.position.x);
+
+            if (distanceToTouch > stopDistance)
+            {
+                Vector3 targetPosition = new Vector3(touchPosition.x, transform.position.y, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, targetPosition, moveSmoothing);
+                input = Mathf.Sign(touchPosition.x - transform.position.x);
+            }
+            else
+            {
+                input = 0f; 
+            }
+        }
+        else
+        {
+            input = 0f;
+        }
+
+        if (input > 0)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        else if (input < 0)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        anim.SetBool("isRunning", input != 0f);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        source.Play();
+        health -= damageAmount;
+        healthDisplay.text = health.ToString();
+
+        if (health <= 0)
+        {
+            loseScreen.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+    public GameObject player;
+
+    private float spawnTimer;
+    private float spawnRate = 5f;
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            SpawnEnemy();
+            spawnTimer = spawnRate;
+            spawnRate = Mathf.Clamp(spawnRate - 0.1f, 1f, 3f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        var randomIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoints[randomIndex].position, Quaternion.identity);
+    }
+}
